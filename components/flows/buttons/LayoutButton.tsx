9d6b594ff56f5cc5e5c0071d91526a1b13890f13
abdellah <Abdellah.chehri14@gmail.com>
2023-02-07 09:24:19 +0100

    add

--- components/flows/buttons/LayoutButton.tsx
+++ components/flows/buttons/LayoutButton.tsx
@@ -1,9 +1,68 @@
-import React from 'react'
 
-function LayoutButton() {
+import { Tab } from '@headlessui/react'
+import GridViewIcon from '@mui/icons-material/GridView';
+import GridOnIcon from '@mui/icons-material/GridOn';
+import TableRowsIcon from '@mui/icons-material/TableRows';
+
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(' ')
+}
+
+export default function LayoutButton() {
+  
+
   return (
-    <div>LayoutButton</div>
+    <div className="w-[200px] max-w-md px-2 py-16 sm:px-0">
+      <Tab.Group>
+        <Tab.List className="flex space-x-1 rounded-xl bg-gray-200/20 p-1">
+          
+            <Tab
+              className={({ selected }) =>
+                classNames(
+                  'w-full m-1 rounded-lg py-2.5 text-sm font-medium leading-5 text-black',
+                  'ring-white    focus:outline-none',
+                  selected
+                    ? 'bg-white shadow'
+                    : 'text-gray-200 hover:bg-white/[0.12] hover:text-black'
+                )
+              }
+            >
+             <GridViewIcon />
+            </Tab>
+             
+            <Tab
+              className={({ selected }) =>
+                classNames(
+                  'w-full m-1 rounded-lg py-2.5 text-sm font-medium leading-5 text-black',
+                  'ring-white    focus:outline-none',
+                  selected
+                    ? 'bg-white shadow'
+                    : 'text-gray-200 hover:bg-white/[0.12] hover:text-black'
+                )
+              }
+            >
+                <GridOnIcon />
+             
+            </Tab>
+             
+            <Tab
+              className={({ selected }) =>
+                classNames(
+                  'w-full m-1 rounded-lg py-2.5 text-sm font-medium leading-5 text-black',
+                  'ring-white    focus:outline-none',
+                  selected
+                    ? 'bg-white shadow'
+                    : 'text-gray-200 hover:bg-white/[0.12] hover:text-black'
+                )
+              }
+            >
+              <TableRowsIcon />
+            </Tab>
+          
+        </Tab.List>
+       
+      </Tab.Group>
+    </div>
   )
 }
 
-export default LayoutButton
--- components/flows/components/PageHeadFlows.tsx
+++ components/flows/components/PageHeadFlows.tsx
@@ -1,14 +1,13 @@
 import React from 'react'
-import GridViewIcon from '@mui/icons-material/GridView';
-import GridOnIcon from '@mui/icons-material/GridOn';
-import TableRowsIcon from '@mui/icons-material/TableRows';
+import LayoutButton from '../buttons/LayoutButton';
+
 
 function PageHead() {
   return (
     <div className='w-full h-[70px] flex justify-between items-center px-4'>
         <h1 className='text-2xl  '>Flows </h1>
         <div className='h-full w-[40%] flex items-center '>
-    
+        <LayoutButton />
         </div>
     </div>
   )
