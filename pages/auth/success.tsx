6f08c3b3bf4df3974bbf0f6407f39ab208d25cc3
abdellah <Abdellah.chehri14@gmail.com>
2023-02-13 22:24:54 +0100

    add

--- pages/auth/success.tsx
+++ pages/auth/success.tsx
@@ -1,8 +1,13 @@
 export default function Succuss() {
     return (
-      <div className='w-full h-screen flex flex-col items-center'>
-          <h1>you have successfully completed the sign up process</h1>
-          <h2>check your email box to confirm your email</h2>
+      <div className='w-full h-screen flex flex-col items-center justify-center'>
+
+          <h1 className="text-3xl font-bold text-blue-600">OpenFlows</h1>
+
+          <h2
+          className="text-xl font-bold text-black"
+          >you have successfully completed the sign up process</h2>
+          <h2 className="text-xl  text-black">check your email box to confirm your email</h2>
       </div>
     )
   }
