5f6067d0fc82efd26dd118272f226053a377ceb5
abdellah <Abdellah.chehri14@gmail.com>
2023-02-27 09:44:33 +0100

    add

--- components/landing/AboutUs.tsx
+++ components/landing/AboutUs.tsx
@@ -5,7 +5,7 @@ export default function AboutUs() {
   
     id="aboutUs" className='w-full  h-[200px] bg-gradient-to-r flex shadow-2xl shadow-blue-700  justify-center items-center from-sky-600 to-indigo-800'>
         <h1 className=' text-xl lg:text-3xl text-white text-center font-bold'>
-        ce project realise par  chehri abdellah acherf saidi salah blill</h1>
+        ce projet réalisé par chehri abdellah acherf saidi salah blill</h1>
     </div>
   )
 }
